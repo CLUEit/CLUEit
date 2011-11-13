67b09778bd38b9a6759f724457d08e5995f8225a
Nisha Pillai <nishpill@gmail.com>
2011-11-13 16:28:23 -0500

    added solution for multiple horizontal tabs for multiple phrase searches

---
+++ CLUEit_WindowsForms_VSProject_MultiplePhraseTabs/CLUEIT2/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace CLUEIT2
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.labelCLUEit = new System.Windows.Forms.Label();
+            this.tBoxSearchTerms = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.tabControlPhrases = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tlpMain.SuspendLayout();
+            this.tabControlPhrases.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpMain.ColumnCount = 3;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 95F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 69F));
+            this.tlpMain.Controls.Add(this.btnUpdate, 2, 0);
+            this.tlpMain.Controls.Add(this.tBoxSearchTerms, 1, 0);
+            this.tlpMain.Controls.Add(this.labelCLUEit, 0, 0);
+            this.tlpMain.Controls.Add(this.tabControlPhrases, 0, 1);
+            this.tlpMain.Location = new System.Drawing.Point(1, 6);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 2;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMain.Size = new System.Drawing.Size(497, 305);
+            this.tlpMain.TabIndex = 0;
+            // 
+            // labelCLUEit
+            // 
+            this.labelCLUEit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelCLUEit.AutoSize = true;
+            this.labelCLUEit.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCLUEit.Location = new System.Drawing.Point(3, 0);
+            this.labelCLUEit.Name = "labelCLUEit";
+            this.labelCLUEit.Size = new System.Drawing.Size(89, 27);
+            this.labelCLUEit.TabIndex = 1;
+            this.labelCLUEit.Text = "CLUEit";
+            // 
+            // tBoxSearchTerms
+            // 
+            this.tBoxSearchTerms.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tBoxSearchTerms.Location = new System.Drawing.Point(98, 3);
+            this.tBoxSearchTerms.Name = "tBoxSearchTerms";
+            this.tBoxSearchTerms.Size = new System.Drawing.Size(327, 20);
+            this.tBoxSearchTerms.TabIndex = 2;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.Location = new System.Drawing.Point(431, 3);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(63, 21);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // tabControlPhrases
+            // 
+            this.tabControlPhrases.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpMain.SetColumnSpan(this.tabControlPhrases, 3);
+            this.tabControlPhrases.Controls.Add(this.tabPage1);
+            this.tabControlPhrases.Controls.Add(this.tabPage2);
+            this.tabControlPhrases.Location = new System.Drawing.Point(3, 30);
+            this.tabControlPhrases.Name = "tabControlPhrases";
+            this.tabControlPhrases.SelectedIndex = 0;
+            this.tabControlPhrases.Size = new System.Drawing.Size(491, 272);
+            this.tabControlPhrases.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(483, 246);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(105, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 311);
+            this.Controls.Add(this.tlpMain);
+            this.MinimumSize = new System.Drawing.Size(516, 349);
+            this.Name = "mainWindow";
+            this.Text = "CLUEit";
+            this.tlpMain.ResumeLayout(false);
+            this.tlpMain.PerformLayout();
+            this.tabControlPhrases.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox tBoxSearchTerms;
+        private System.Windows.Forms.Label labelCLUEit;
+        private System.Windows.Forms.TabControl tabControlPhrases;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ CLUEit_WindowsForms_VSProject_MultiplePhraseTabs/CLUEIT2/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CLUEIT2
+{
+    public partial class mainWindow : Form
+    {
+        public mainWindow(string[] args)
+        {
+            InitializeComponent();
+           // tabControlPhrases.SelectedTab = tabControlPhrases.TabPages[1];
+            tabControlPhrases.TabPages.Remove(tabControlPhrases.TabPages[1]);
+            tabControlPhrases.TabPages.Remove(tabControlPhrases.TabPages[0]);
+
+           /* string[] words = parseInitialInput(args);
+            tBoxSearchTerms.Text = string.Join<string>(" ", words);
+
+            tabControlMain.TabPages[0].Text = "Wikipedia";
+            loadWikipediaTab(words);
+            //webBrowserWikipedia.Navigate("www.wikipedia.org");
+
+            tabControlMain.TabPages[1].Text = "Google";
+            loadGoogleTab(words);
+            //webBrowserGoogle.Navigate("www.google.com");
+
+            tabControlMain.TabPages[2].Text = "Images";
+            loadImagesTab(words);
+            //webBrowserImages.Navigate("www.google.com/imghp?hl=en&tab=wi");
+
+            tabControlMain.TabPages[3].Text = "YouTube";
+            loadYouTubeTab(words);
+            //webBrowserImages.Navigate("www.youtube.com");*/
+        }
+
+        void makeNewPhraseTab(string[] args)
+        {
+            string searchPart = string.Join<string>(" ", args);
+            //maybe jsut call join instead of looping elsewhere
+
+            tabControlPhrases.TabPages.Add(searchPart);
+            TabControl searches = new TabControl();
+            searches.Parent = tabControlPhrases.TabPages[tabControlPhrases.TabCount - 1];
+            searches.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right
+                | AnchorStyles.Top | AnchorStyles.Left);
+            searches.Size = searches.Parent.Size;
+
+            searches.TabPages.Add("Wikipedia");
+            WebBrowser wikipediaBrowser = new WebBrowser();
+            wikipediaBrowser.Parent = searches.TabPages[searches.TabCount-1];
+            wikipediaBrowser.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right
+                | AnchorStyles.Top | AnchorStyles.Left);
+            //searches.Show();
+
+           // Form form = new Form();
+            //searches.Parent = form;
+            //form.Show();
+         //   searches.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right
+           //         | AnchorStyles.Top | AnchorStyles.Left);
+
+           // form.Show();
+
+            loadWikipediaTab(args, wikipediaBrowser);
+
+            searches.TabPages.Add("Google");
+            WebBrowser googleBrowser = new WebBrowser();
+            googleBrowser.Parent = searches.TabPages[searches.TabCount - 1];
+            googleBrowser.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right
+                | AnchorStyles.Top | AnchorStyles.Left);
+            loadGoogleTab(args, googleBrowser);
+
+            searches.TabPages.Add("Images");
+            WebBrowser imagesBrowser = new WebBrowser();
+            imagesBrowser.Parent = searches.TabPages[searches.TabCount - 1];
+            imagesBrowser.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right
+                | AnchorStyles.Top | AnchorStyles.Left);
+            loadImagesTab(args, imagesBrowser);
+
+            searches.TabPages.Add("YouTube");
+            WebBrowser youTubeBrowser = new WebBrowser();
+            youTubeBrowser.Parent = searches.TabPages[searches.TabCount - 1];
+            //youTubeBrowser.Size = (10,10);
+            youTubeBrowser.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right
+                | AnchorStyles.Top | AnchorStyles.Left);
+            loadYouTubeTab(args, youTubeBrowser);
+
+        }
+
+
+         private void loadWikipediaTab(string[] args, WebBrowser browser)
+        {
+            string searchPart = "";
+            foreach (string arg in args)
+            {
+                searchPart += arg + "_";
+            }
+             
+             browser.Navigate("http://en.wikipedia.org/wiki/" + searchPart);
+            //webBrowserWikipedia.Navigate("http://en.wikipedia.org/wiki/" + searchPart);
+        }
+
+        private void loadGoogleTab(string[] args, WebBrowser browser)
+        {
+            string searchPart = "";
+            foreach (string arg in args)
+            {
+                searchPart += arg + "+";
+            }
+
+            browser.Navigate("https://www.google.com/search?rlz=1C1CHFX_enUS454US454&gcx=w&ix=c1&sourceid=chrome&ie=UTF-8&q="
+            + searchPart);
+           // webBrowserGoogle.Navigate("https://www.google.com/search?rlz=1C1CHFX_enUS454US454&gcx=w&ix=c1&sourceid=chrome&ie=UTF-8&q="
+           // + searchPart);
+        }
+
+        private void loadImagesTab(string[] args, WebBrowser browser)
+        {
+            string searchPart = "";
+            foreach (string arg in args)
+            {
+                searchPart += arg + "+";
+            }
+
+            browser.Navigate("http://www.google.com/search?tbm=isch&hl=en&source=hp&biw=1366&bih=635&q="
+            + searchPart
+            + "&gbv=2&oq="
+            + searchPart
+            + "&aq=f&aqi=g5g-m5&aql=1&gs_sm=e&gs_upl=1761l2788l0l2849l8l3l0l0l0l0l137l371l0.3l3l0");
+           // webBrowserImages.Navigate("http://www.google.com/search?tbm=isch&hl=en&source=hp&biw=1366&bih=635&q="
+            //+ searchPart
+            //+ "&gbv=2&oq="
+            //+ searchPart
+            //+ "&aq=f&aqi=g5g-m5&aql=1&gs_sm=e&gs_upl=1761l2788l0l2849l8l3l0l0l0l0l137l371l0.3l3l0");
+
+             //webBrowserWikipedia.Navigate("http://www.google.com/search?tbm=isch&hl=en&source=hp&biw=1366&bih=674&gbv=2&oq=" 
+                  //  + searchPart
+                    //+ "&aq=f&aqi=g10&gs_upl=2303120l2305059l0l2306057l12l11l0l2l2l0l384l1587l1.5.2.1l9l0&q=vincent%20van&orq=vincent+van+");
+        }
+
+        private void loadYouTubeTab(string[] args, WebBrowser browser)
+        {
+            string searchPart = "";
+            foreach (string arg in args)
+            {
+                searchPart += arg + "+";
+            }
+
+            browser.Navigate("http://www.youtube.com/results?search_query="
+            + searchPart
+            + "&aq=f");
+          //  webBrowserYouTube.Navigate("http://www.youtube.com/results?search_query="
+            //+ searchPart
+            //+ "&aq=f");
+        }
+
+        private void loadDictionaryTab(string[] args)
+        {
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (tBoxSearchTerms.Text == "")
+            {
+                MessageBox.Show("No search terms were entered");
+                return;
+            }
+
+            makeNewPhraseTab(tBoxSearchTerms.Text.Split(' '));
+            /*string[] args = tBoxSearchTerms.Text.Split(' ');
+            loadWikipediaTab(args);
+            loadGoogleTab(args);
+            loadImagesTab(args);
+            loadYouTubeTab(args);
+            loadDictionaryTab(args);*/
+        }
+
+
+
+
+    
+
+
+
+
+
+
+    }
+}
