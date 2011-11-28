c6680f467eef3339e735d8c39cf32431fc30cb2c
Nisha Pillai <nishpill@gmail.com>
2011-11-28 05:56:57 -0500

    vertical tabs, larger tab header text, CLUEit icon added, settings form added

---
+++ CLUEit_toIntegrateWithLou/CLUEIT2/MainForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace CLUEIT2
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWindow));
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.tBoxSearchTerms = new System.Windows.Forms.TextBox();
+            this.labelCLUEit = new System.Windows.Forms.Label();
+            this.tabControlPhrases = new System.Windows.Forms.TabControl();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.toolTipSettings = new System.Windows.Forms.ToolTip(this.components);
+            this.tlpMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpMain.ColumnCount = 4;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 95F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 69F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tlpMain.Controls.Add(this.tBoxSearchTerms, 1, 0);
+            this.tlpMain.Controls.Add(this.labelCLUEit, 0, 0);
+            this.tlpMain.Controls.Add(this.btnSettings, 3, 0);
+            this.tlpMain.Controls.Add(this.btnUpdate, 2, 0);
+            this.tlpMain.Controls.Add(this.tabControlPhrases, 0, 1);
+            this.tlpMain.Location = new System.Drawing.Point(1, 6);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 2;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpMain.Size = new System.Drawing.Size(497, 305);
+            this.tlpMain.TabIndex = 0;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.Location = new System.Drawing.Point(395, 10);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(63, 22);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // tBoxSearchTerms
+            // 
+            this.tBoxSearchTerms.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tBoxSearchTerms.Location = new System.Drawing.Point(98, 12);
+            this.tBoxSearchTerms.Name = "tBoxSearchTerms";
+            this.tBoxSearchTerms.Size = new System.Drawing.Size(291, 20);
+            this.tBoxSearchTerms.TabIndex = 2;
+            // 
+            // labelCLUEit
+            // 
+            this.labelCLUEit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelCLUEit.AutoSize = true;
+            this.labelCLUEit.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCLUEit.Location = new System.Drawing.Point(3, 10);
+            this.labelCLUEit.Name = "labelCLUEit";
+            this.labelCLUEit.Size = new System.Drawing.Size(89, 25);
+            this.labelCLUEit.TabIndex = 1;
+            this.labelCLUEit.Text = "CLUEit";
+            // 
+            // tabControlPhrases
+            // 
+            this.tabControlPhrases.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.tabControlPhrases.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpMain.SetColumnSpan(this.tabControlPhrases, 4);
+            this.tabControlPhrases.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControlPhrases.Location = new System.Drawing.Point(3, 38);
+            this.tabControlPhrases.Multiline = true;
+            this.tabControlPhrases.Name = "tabControlPhrases";
+            this.tabControlPhrases.SelectedIndex = 0;
+            this.tabControlPhrases.Size = new System.Drawing.Size(491, 264);
+            this.tabControlPhrases.TabIndex = 5;
+            this.tabControlPhrases.Selecting += new System.Windows.Forms.TabControlCancelEventHandler(this.tabControlPhrases_Selecting);
+            this.tabControlPhrases.Deselecting += new System.Windows.Forms.TabControlCancelEventHandler(this.tabControlPhrases_Deselecting);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSettings.BackgroundImage = global::CLUEIT2.Properties.Resources.settingsIcon;
+            this.btnSettings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSettings.Location = new System.Drawing.Point(464, 3);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(30, 29);
+            this.btnSettings.TabIndex = 6;
+            this.toolTipSettings.SetToolTip(this.btnSettings, "Settings");
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            // 
+            // mainWindow
+            // 
+            this.AcceptButton = this.btnUpdate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 311);
+            this.Controls.Add(this.tlpMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(516, 349);
+            this.Name = "mainWindow";
+            this.Text = "CLUEit";
+            this.tlpMain.ResumeLayout(false);
+            this.tlpMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox tBoxSearchTerms;
+        private System.Windows.Forms.Label labelCLUEit;
+        private System.Windows.Forms.TabControl tabControlPhrases;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.ToolTip toolTipSettings;
+    }
+}
+
---
+++ CLUEit_toIntegrateWithLou/CLUEIT2/SettingsForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace CLUEIT2
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
+            this.tlpSettings = new System.Windows.Forms.TableLayoutPanel();
+            this.labelDefaultNote = new System.Windows.Forms.Label();
+            this.dgvChooseSettings = new System.Windows.Forms.DataGridView();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.webService = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.defaultS = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.alwaysDisplayS = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.neverDisplayS = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.tlpSettings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChooseSettings)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tlpSettings
+            // 
+            this.tlpSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpSettings.ColumnCount = 2;
+            this.tlpSettings.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpSettings.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 96F));
+            this.tlpSettings.Controls.Add(this.labelDefaultNote, 0, 1);
+            this.tlpSettings.Controls.Add(this.dgvChooseSettings, 0, 0);
+            this.tlpSettings.Controls.Add(this.btnOK, 0, 2);
+            this.tlpSettings.Controls.Add(this.btnCancel, 1, 2);
+            this.tlpSettings.Location = new System.Drawing.Point(-1, 0);
+            this.tlpSettings.Name = "tlpSettings";
+            this.tlpSettings.RowCount = 3;
+            this.tlpSettings.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpSettings.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tlpSettings.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tlpSettings.Size = new System.Drawing.Size(374, 286);
+            this.tlpSettings.TabIndex = 0;
+            // 
+            // labelDefaultNote
+            // 
+            this.labelDefaultNote.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelDefaultNote.AutoSize = true;
+            this.tlpSettings.SetColumnSpan(this.labelDefaultNote, 2);
+            this.labelDefaultNote.Location = new System.Drawing.Point(3, 213);
+            this.labelDefaultNote.Name = "labelDefaultNote";
+            this.labelDefaultNote.Size = new System.Drawing.Size(368, 48);
+            this.labelDefaultNote.TabIndex = 0;
+            this.labelDefaultNote.Text = "Note: Selecting \"Default\" means that the corresponding web service tab will only " +
+                "display if CLUEit\'s algorithms determine it is relevant for the text selected.";
+            // 
+            // dgvChooseSettings
+            // 
+            this.dgvChooseSettings.AllowDrop = true;
+            this.dgvChooseSettings.AllowUserToAddRows = false;
+            this.dgvChooseSettings.AllowUserToDeleteRows = false;
+            this.dgvChooseSettings.AllowUserToResizeColumns = false;
+            this.dgvChooseSettings.AllowUserToResizeRows = false;
+            this.dgvChooseSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvChooseSettings.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvChooseSettings.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvChooseSettings.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.webService,
+            this.defaultS,
+            this.alwaysDisplayS,
+            this.neverDisplayS});
+            this.tlpSettings.SetColumnSpan(this.dgvChooseSettings, 2);
+            this.dgvChooseSettings.Location = new System.Drawing.Point(3, 3);
+            this.dgvChooseSettings.Name = "dgvChooseSettings";
+            this.dgvChooseSettings.RowHeadersVisible = false;
+            this.dgvChooseSettings.Size = new System.Drawing.Size(368, 207);
+            this.dgvChooseSettings.TabIndex = 1;
+            this.dgvChooseSettings.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvChooseSettings_CellValueChanged);
+            this.dgvChooseSettings.CurrentCellChanged += new System.EventHandler(this.dgvChooseSettings_CurrentCellChanged);
+            this.dgvChooseSettings.CurrentCellDirtyStateChanged += new System.EventHandler(this.dgvChooseSettings_CurrentCellDirtyStateChanged);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(200, 264);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 19);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(296, 264);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 19);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // webService
+            // 
+            this.webService.FillWeight = 110F;
+            this.webService.HeaderText = "Web Service";
+            this.webService.Name = "webService";
+            this.webService.ReadOnly = true;
+            // 
+            // defaultS
+            // 
+            this.defaultS.FillWeight = 65F;
+            this.defaultS.HeaderText = "Default";
+            this.defaultS.Name = "defaultS";
+            this.defaultS.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.defaultS.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // alwaysDisplayS
+            // 
+            this.alwaysDisplayS.FillWeight = 112F;
+            this.alwaysDisplayS.HeaderText = "Always Display";
+            this.alwaysDisplayS.Name = "alwaysDisplayS";
+            this.alwaysDisplayS.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.alwaysDisplayS.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // neverDisplayS
+            // 
+            this.neverDisplayS.FillWeight = 110F;
+            this.neverDisplayS.HeaderText = "Never Display";
+            this.neverDisplayS.Name = "neverDisplayS";
+            this.neverDisplayS.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.neverDisplayS.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // SettingsForm
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(384, 294);
+            this.Controls.Add(this.tlpSettings);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(390, 322);
+            this.MinimumSize = new System.Drawing.Size(390, 322);
+            this.Name = "SettingsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Settings";
+            this.tlpSettings.ResumeLayout(false);
+            this.tlpSettings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChooseSettings)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpSettings;
+        private System.Windows.Forms.Label labelDefaultNote;
+        private System.Windows.Forms.DataGridView dgvChooseSettings;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn webService;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn defaultS;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn alwaysDisplayS;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn neverDisplayS;
+
+    }
+}
